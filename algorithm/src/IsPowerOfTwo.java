45ff201636fba3745e8b2747cc8ee6fa84174173
lihua <zhanglihua@ingbaobei.com>
2021-10-14 15:36:14 +0800

    231-2的幂-位运算

--- algorithm/src/IsPowerOfTwo.java
+++ algorithm/src/IsPowerOfTwo.java
@@ -5,20 +5,7 @@
 public class IsPowerOfTwo {
 
 	public static boolean isPowerOfTwo(int n) {
-		if (n <= 0) {
-			return false;
-		}
-		if (n == 1) {
-			return true;
-		}
-		boolean flag = true;
-		do {
-			if (n % 2 != 0) {
-				flag = false;
-				break;
-			}
-			n = n >> 1;
-		} while (n != 1);
-		return flag;
+		return n > 0
+				&& (n & (n - 1)) == 0;
 	}
 }
