db09dcf975b46974dda47c20ca9b591756e000c3
zhangzhang <zhanglihua_scnu@yeah.net>
2021-10-16 12:59:06 +0800

    101-对称二叉树

---
+++ algorithm/src/IsSymmetric.java
@@ -0,0 +1,78 @@
+//给定一个二叉树，检查它是否是镜像对称的。
+//
+//
+//
+// 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+//
+//     1
+//   / \
+//  2   2
+// / \ / \
+//3  4 4  3
+//
+//
+//
+//
+// 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+//
+//     1
+//   / \
+//  2   2
+//   \   \
+//   3    3
+//
+//
+//
+//
+// 进阶：
+//
+// 你可以运用递归和迭代两种方法解决这个问题吗？
+
+import java.util.Objects;
+
+/**
+ * @author lihua
+ * @since 2021/10/16
+ */
+public class IsSymmetric {
+
+    public boolean isSymmetric(TreeNode root) {
+        if (Objects.isNull(root)) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode p, TreeNode q) {
+        if (Objects.isNull(p) && Objects.isNull(q)) {
+            return true;
+        }
+        if (Objects.isNull(p) || Objects.isNull(q)) {
+            return false;
+        }
+        return p.val == q.val
+                && isSymmetric(p.left, q.right)
+                && isSymmetric(p.right, q.left);
+    }
+
+    public static class TreeNode {
+        int val;
+
+        TreeNode left;
+
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
