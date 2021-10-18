b9adcce2b82a0245fcbf736657d247c42577e881
lihua <zhanglihua@ingbaobei.com>
2021-10-18 11:40:06 +0800

    141-环形链表

---
+++ algorithm/src/structure/ListNode.java
@@ -0,0 +1,17 @@
+package structure;
+
+/**
+ * @author lihua
+ * @since 2021/10/18
+ */
+public class ListNode {
+
+	public int val;
+
+	public ListNode next;
+
+	ListNode(int x) {
+		val = x;
+		next = null;
+	}
+}
