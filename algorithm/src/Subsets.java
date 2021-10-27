7e349751ca97aa5a2919de45e6ca0c13a01fc300
lihua <zhanglihua@ingbaobei.com>
2021-10-27 16:50:47 +0800

    78-子集-for版

--- algorithm/src/Subsets.java
+++ algorithm/src/Subsets.java
@@ -1,6 +1,36 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+//给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+//
+// 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [1,2,3]
+//输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [0]
+//输出：[[],[0]]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 10
+// -10 <= nums[i] <= 10
+// nums 中的所有元素 互不相同
+//
+// Related Topics 位运算 数组 回溯
 
 /**
  * @author lihua
@@ -17,17 +47,12 @@ public class Subsets {
     }
 
     private void backtrack(int[] nums, int index, LinkedList<Integer> track) {
-        // 遍历完整个数组之后，就可以将路径放进结果集了
-        if (index == nums.length) {
-            resultList.add(new LinkedList<>(track));
-            return;
+        resultList.add(new LinkedList<>(track));
+        for (int i = index; i < nums.length; i++) {
+            track.add(nums[i]);
+            backtrack(nums, index + 1, track);
+            track.removeLast();
         }
-        // 选择index所在位置的元素的情况
-        track.add(nums[index]);
-        backtrack(nums, index + 1, track);
-        track.removeLast();
-        // 不选择index所在位置的元素的情况
-        backtrack(nums, index + 1, track);
     }
 
     public static void main(String[] args) {
