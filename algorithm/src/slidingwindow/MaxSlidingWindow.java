0871bec5a40cd60a596ed293ac20d0e41ade2efb
lihua <zhanglihua@ingbaobei.com>
2021-11-06 15:10:59 +0800

    239-滑动窗口最大值

--- algorithm/src/slidingwindow/MaxSlidingWindow.java
+++ algorithm/src/slidingwindow/MaxSlidingWindow.java
@@ -62,40 +62,37 @@ package slidingwindow;
 //
 // Related Topics 队列 数组 滑动窗口 单调队列 堆（优先队列）
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.PriorityQueue;
 
 /**
+ * n个数，k大小的滑动窗口，会有n-k+1个滑动窗口
+ *
  * @author lihua
  * @since 2021/11/6
  */
 public class MaxSlidingWindow {
 
-    private List<Integer> resultList = new ArrayList<>();
-
     public int[] maxSlidingWindow(int[] nums, int k) {
-        int left = 0, right = 0;
         int length = nums.length;
-        int loop;
-        int num;
-        while (right < length) {
-            while (right - left + 1 >= k) {
-                if (right - left + 1 == k) {
-                    loop = left;
-                    num = Integer.MIN_VALUE;
-                    while (loop <= right) {
-                        num = Math.max(num, nums[loop]);
-                        loop++;
-                    }
-                    resultList.add(num);
-                }
-                left++;
-            }
-            right++;
+        int[] results = new int[length - k + 1];
+        // 用大根堆实现
+        PriorityQueue<int[]> queue = new PriorityQueue<>((arr1, arr2) -> arr2[0] - arr1[0]);
+        // 预先初始化第一个滑动窗口的元素进堆中，后续每次只处理一个元素，即不断向右滑动
+        for (int i = 0; i < k; i++) {
+            queue.add(new int[]{nums[i], i});
         }
-        return resultList.stream()
-                .mapToInt(i -> i)
-                .toArray();
+        // 这里是peek而不是poll：每次取最大值，都不应该从堆中移除
+        results[0] = queue.peek()[0];
+        for (int i = k; i < length; i++) {
+            queue.add(new int[]{nums[i], i});
+            // 当堆顶元素已经不在滑动窗口中时，把它移除
+            while (queue.peek()[1] <= i - k) {
+                queue.poll();
+            }
+            // 此时仍在堆顶的元素，即为当前滑动窗口中最大的元素
+            results[i - k + 1] = queue.peek()[0];
+        }
+        return results;
     }
 
     public static void main(String[] args) {
