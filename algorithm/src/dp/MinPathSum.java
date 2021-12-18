9aeb7b134540ad7c26d3cafef6097b4959778210
lihua <zhanglihua@ingbaobei.com>
2021-12-18 17:00:21 +0800

    64-最小路径和-迭代+预先填表

--- algorithm/src/dp/MinPathSum.java
+++ algorithm/src/dp/MinPathSum.java
@@ -35,8 +35,6 @@ package dp;
 //
 // Related Topics 数组 动态规划 矩阵
 
-import java.util.Arrays;
-
 /**
  * "一般来说，在二维矩阵中求最优化问题（最大值或者最小值），肯定需要递归 + 备忘录，也就是动态规划技巧。"
  * 重要的是，确定好dp数组的定义，在此基础上定义状态转移方程和遍历方向
@@ -45,8 +43,6 @@ import java.util.Arrays;
  * @since 2021/12/18
  */
 public class MinPathSum {
-
-    private static final int INITIAL_NUM = 66666;
 
     /**
      * sum[row][col]表示：
@@ -61,34 +57,28 @@ public class MinPathSum {
     public int minPathSum(int[][] grid) {
         rowLength = grid.length;
         colLength = grid[0].length;
-        initializeSum();
+        sum = new int[rowLength][colLength];
         find(grid);
         return sum[rowLength - 1][colLength - 1];
     }
 
     private void find(int[][] grid) {
-        for (int row = 0; row < rowLength; row++) {
-            for (int col = 0; col < colLength; col++) {
-                // base case是起点[0,0]
-                if (row == 0 && col == 0) {
-                    sum[0][0] = grid[0][0];
-                    continue;
-                }
+        // base case是起点[0,0]
+        sum[0][0] = grid[0][0];
+        for (int i = 1; i < colLength; i++) {
+            sum[0][i] = sum[0][i - 1] + grid[0][i];
+        }
+        for (int j = 1; j < rowLength; j++) {
+            sum[j][0] = sum[j - 1][0] + grid[j][0];
+        }
+        for (int row = 1; row < rowLength; row++) {
+            for (int col = 1; col < colLength; col++) {
                 // 反L型
-                // 如果列是0，说明是从上面走下来的
-                int left = col >= 1 ? sum[row][col - 1] : INITIAL_NUM;
-                // 如果行是0，说明是说左边走过来的
-                int up = row >= 1 ? sum[row - 1][col] : INITIAL_NUM;
+                int left = sum[row][col - 1];
+                int up = sum[row - 1][col];
                 int currentMinSum = grid[row][col] + Math.min(left, up);
                 sum[row][col] = currentMinSum;
             }
-        }
-    }
-
-    private void initializeSum() {
-        sum = new int[rowLength][colLength];
-        for (int i = 0; i < rowLength; i++) {
-            Arrays.fill(sum[i], INITIAL_NUM);
         }
     }
 
@@ -99,8 +89,8 @@ public class MinPathSum {
     public static void main(String[] args) {
         MinPathSum clazz = new MinPathSum();
         // 1→3→1→1→1
-        int[][] grid = new int[][]{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
+        int[][] grid = new int[][]{{1, 2, 3}, {4, 5, 6}};
         int result = clazz.minPathSum(grid);
-        assert result == 7;
+        assert result == 12;
     }
 }
