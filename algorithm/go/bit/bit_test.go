4efc743a25ce238124d56aa39a466bba6234e8a7
lihua <zhanglihua@ingbaobei.com>
2022-05-07 13:48:32 +0800

    136-只出现一次的数字II
    136-只出现一次的数字II

--- algorithm/go/bit/bit_test.go
+++ algorithm/go/bit/bit_test.go
@@ -9,3 +9,8 @@ func TestSingleNumber(t *testing.T) {
 	structure.Validate(SingleNumber([]int{2, 2, 1}), 1, t)
 	structure.Validate(SingleNumber([]int{4, 1, 2, 1, 2}), 4, t)
 }
+
+func TestSingleNumberII(t *testing.T) {
+	structure.Validate(SingleNumberII([]int{2, 2, 3, 2}), 3, t)
+	structure.Validate(SingleNumberII([]int{0, 1, 0, 1, 0, 1, 99}), 99, t)
+}
