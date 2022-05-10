d4a27b0bcfb9a0ef46457fce887471aad37bc407
lihua <zhanglihua@ingbaobei.com>
2022-05-10 10:47:41 +0800

    11-盛最多水的容器

--- algorithm/go/structure/common.go
+++ algorithm/go/structure/common.go
@@ -1,9 +1,19 @@
 package structure
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func Validate(actual any, expect any, t *testing.T) {
 	if actual != expect {
+		t.Errorf("test failed, expect value is: %#v, acutal value is: %#v", expect, actual)
+	}
+}
+
+// ValidateDeep 引用类型如切片、字典的比较
+func ValidateDeep(actual any, expect any, t *testing.T) {
+	if !reflect.DeepEqual(actual, expect) {
 		t.Errorf("test failed, expect value is: %#v, acutal value is: %#v", expect, actual)
 	}
 }
