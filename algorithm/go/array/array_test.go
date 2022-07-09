792b0d6809180690b0a19d696b089014436ed267
zhanglihua.river <zhanglihua.river@bytedance.com>
2022-07-09 21:05:20 +0800

    35-搜索插入位置
    35-搜索插入位置

--- algorithm/go/array/array_test.go
+++ algorithm/go/array/array_test.go
@@ -11,3 +11,14 @@ func TestSearchRange(t *testing.T) {
 	res = searchRangeOptimize([]int{}, 0)
 	structure.Validate(res, []int{-1, -1}, t)
 }
+
+func TestSearchInsert(t *testing.T) {
+	res := searchInsert([]int{1, 3, 5, 6}, 5)
+	structure.Validate(res, 2, t)
+	res = searchInsert([]int{1, 3, 5, 6}, 2)
+	structure.Validate(res, 1, t)
+	res = searchInsert([]int{1, 3, 5, 6}, 7)
+	structure.Validate(res, 4, t)
+	res = searchInsert([]int{1, 3, 5, 6}, 0)
+	structure.Validate(res, 0, t)
+}
