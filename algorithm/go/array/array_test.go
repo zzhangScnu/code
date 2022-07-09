3545cb1d7a87f29158141186b66311cb031cbca4
zhanglihua.river <zhanglihua.river@bytedance.com>
2022-07-09 12:45:47 +0800

    704-二分查找

--- algorithm/go/array/array_test.go
+++ algorithm/go/array/array_test.go
@@ -1,19 +1,13 @@
 package main
 
 import (
-	"reflect"
+	"algorithm.com/structure"
 	"testing"
 )
 
 func TestSearchRange(t *testing.T) {
 	res := searchRangeOptimize([]int{5, 7, 7, 8, 8, 10}, 6)
-	exp := []int{-1, -1}
-	if !reflect.DeepEqual(res, exp) {
-		t.Errorf("test failed, actual result is %v", res)
-	}
+	structure.ValidateDeep(res, []int{-1, -1}, t)
 	res = searchRangeOptimize([]int{}, 0)
-	exp = []int{-1, -1}
-	if !reflect.DeepEqual(res, exp) {
-		t.Errorf("test failed, actual result is %v", res)
-	}
+	structure.Validate(res, []int{-1, -1}, t)
 }
