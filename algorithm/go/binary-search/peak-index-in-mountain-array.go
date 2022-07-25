967e07dec975fc23dfcef5ea50c59ffca018c0c9
zhanglihua.river <zhanglihua.river@bytedance.com>
2022-07-25 20:52:48 +0800

    852-山脉数组的峰顶索引

---
+++ algorithm/go/binary-search/peak-index-in-mountain-array.go
@@ -0,0 +1,93 @@
+package binary_search
+
+import "math"
+
+//符合下列属性的数组 arr 称为 山脉数组 ：
+//
+// arr.length >= 3
+// 存在 i（0 < i < arr.length - 1）使得：
+//
+// arr[0] < arr[1] < ... arr[i-1] < arr[i]
+// arr[i] > arr[i+1] > ... > arr[arr.length - 1]
+//
+//
+//
+//
+// 给你由整数组成的山脉数组 arr ，返回任何满足 arr[0] < arr[1] < ... arr[i - 1] < arr[i] > arr[i +
+//1] > ... > arr[arr.length - 1] 的下标 i 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：arr = [0,1,0]
+//输出：1
+//
+//
+// 示例 2：
+//
+//
+//输入：arr = [0,2,1,0]
+//输出：1
+//
+//
+// 示例 3：
+//
+//
+//输入：arr = [0,10,5,2]
+//输出：1
+//
+//
+// 示例 4：
+//
+//
+//输入：arr = [3,4,5,1]
+//输出：2
+//
+//
+// 示例 5：
+//
+//
+//输入：arr = [24,69,100,99,79,78,67,36,26,19]
+//输出：2
+//
+//
+//
+//
+// 提示：
+//
+//
+// 3 <= arr.length <= 10⁴
+// 0 <= arr[i] <= 10⁶
+// 题目数据保证 arr 是一个山脉数组
+//
+//
+//
+//
+// 进阶：很容易想到时间复杂度 O(n) 的解决方案，你可以设计一个 O(log(n)) 的解决方案吗？
+// Related Topics 数组 二分查找
+
+func peakIndexInMountainArray(arr []int) int {
+	max := len(arr) - 1
+	lo, hi, mi := 0, max, 0
+	for lo <= hi {
+		mi = lo + (hi-lo)>>1
+		if getN(arr, mi) > getN(arr, mi-1) && getN(arr, mi) > getN(arr, mi+1) {
+			return mi
+		} else if getN(arr, mi) < getN(arr, mi+1) { // 在上坡
+			lo = mi + 1
+		} else { // 在下坡
+			hi = mi - 1
+		}
+	}
+	return -1
+}
+
+// 取数，避免在if条件里加很多额外的判断
+func getN(nums []int, i int) int {
+	if i < 0 || i >= len(nums) {
+		return math.MinInt64
+	}
+	return nums[i]
+}
