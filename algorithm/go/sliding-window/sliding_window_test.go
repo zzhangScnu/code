76ae7f993625bd6fff41449b910a487e61ae3e72
zhanglihua.river <zhanglihua.river@bytedance.com>
2022-07-26 11:37:53 +0800

    3-无重复字符的最长子串

---
+++ algorithm/go/sliding-window/sliding_window_test.go
@@ -0,0 +1,13 @@
+package sliding_window
+
+import (
+	"algorithm.com/structure"
+	"testing"
+)
+
+func TestLengthOfLongestSubstring(t *testing.T) {
+	structure.Validate(lengthOfLongestSubstring("abcabcbb"), 3, t)
+	structure.Validate(lengthOfLongestSubstring("bbbbb"), 1, t)
+	structure.Validate(lengthOfLongestSubstring("pwwkew"), 3, t)
+	structure.Validate(lengthOfLongestSubstring("aab"), 2, t)
+}
