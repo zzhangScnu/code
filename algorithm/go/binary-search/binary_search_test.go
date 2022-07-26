6609fbc6a1b74e7b4b7a6b9642f2320c6cd4d3cf
zhanglihua.river <zhanglihua.river@bytedance.com>
2022-07-26 11:30:58 +0800

    feat: reformat

--- algorithm/go/binary-search/binary_search_test.go
+++ algorithm/go/binary-search/binary_search_test.go
@@ -6,6 +6,24 @@ import (
 	"testing"
 )
 
+func TestSearchRange(t *testing.T) {
+	res := searchRangeOptimize([]int{5, 7, 7, 8, 8, 10}, 6)
+	structure.ValidateDeep(res, []int{-1, -1}, t)
+	res = searchRangeOptimize([]int{}, 0)
+	structure.ValidateDeep(res, []int{-1, -1}, t)
+}
+
+func TestSearchInsert(t *testing.T) {
+	res := searchInsert([]int{1, 3, 5, 6}, 5)
+	structure.Validate(res, 2, t)
+	res = searchInsert([]int{1, 3, 5, 6}, 2)
+	structure.Validate(res, 1, t)
+	res = searchInsert([]int{1, 3, 5, 6}, 7)
+	structure.Validate(res, 4, t)
+	res = searchInsert([]int{1, 3, 5, 6}, 0)
+	structure.Validate(res, 0, t)
+}
+
 func TestMaxEnvelopesDp(t *testing.T) {
 	res := maxEnvelopesByLis([][]int{{5, 4}, {6, 4}, {6, 7}, {2, 3}})
 	structure.Validate(res, 3, t)
