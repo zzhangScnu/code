bc7a2f1aad7c55cc7f64692316a4b197cc94c126
zhanglihua.river <zhanglihua.river@bytedance.com>
2022-09-20 21:14:00 +0800

    1352-最后K个数的乘积

---
+++ algorithm/go/pre_sum/count-range-sum.go
@@ -0,0 +1,40 @@
+package pre_sum
+
+//给你一个整数数组 nums 以及两个整数 lower 和 upper 。求数组中，值位于范围 [lower, upper] （包含 lower 和
+//upper）之内的 区间和的个数 。
+//
+// 区间和 S(i, j) 表示在 nums 中，位置从 i 到 j 的元素之和，包含 i 和 j (i ≤ j)。
+//
+//
+//示例 1：
+//
+//
+//输入：nums = [-2,5,-1], lower = -2, upper = 2
+//输出：3
+//解释：存在三个区间：[0,0]、[2,2] 和 [0,2] ，对应的区间和分别是：-2 、-1 、2 。
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [0], lower = 0, upper = 0
+//输出：1
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 10⁵
+// -2³¹ <= nums[i] <= 2³¹ - 1
+// -10⁵ <= lower <= upper <= 10⁵
+// 题目数据保证答案是一个 32 位 的整数
+//
+//
+// Related Topics 树状数组 线段树 数组 二分查找 分治 有序集合 归并排序
+
+func countRangeSum(nums []int, lower int, upper int) int { // 327
+	// todo： 二叉树纲领 + 归并
+	return 0
+}
--- algorithm/go/pre_sum/pre_sum_test.go
+++ algorithm/go/pre_sum/pre_sum_test.go
@@ -2,10 +2,25 @@ package pre_sum
 
 import (
 	. "algorithm.com/structure"
+	"fmt"
 	"testing"
 )
 
 func TestSumRegion(t *testing.T) {
 	numMatrix := Constructor1([][]int{{3, 0, 1, 4, 2}, {5, 6, 3, 2, 1}, {1, 2, 0, 1, 5}, {4, 1, 0, 1, 7}, {1, 0, 3, 0, 5}})
 	Validate(numMatrix.SumRegion(2, 1, 4, 3), 8, t)
+}
+
+func TestGetProduct(t *testing.T) {
+	productOfNumbers := Constructor2()
+	productOfNumbers.Add(3)                     // [3]
+	productOfNumbers.Add(0)                     // [3,0]
+	productOfNumbers.Add(2)                     // [3,0,2]
+	productOfNumbers.Add(5)                     // [3,0,2,5]
+	productOfNumbers.Add(4)                     // [3,0,2,5,4]
+	fmt.Println(productOfNumbers.GetProduct(2)) // 返回 20 。最后 2 个数字的乘积是 5 * 4 = 20
+	fmt.Println(productOfNumbers.GetProduct(3)) // 返回 40 。最后 3 个数字的乘积是 2 * 5 * 4 = 40
+	fmt.Println(productOfNumbers.GetProduct(4)) // 返回  0 。最后 4 个数字的乘积是 0 * 2 * 5 * 4 = 0
+	productOfNumbers.Add(8)                     // [3,0,2,5,4,8]
+	fmt.Println(productOfNumbers.GetProduct(2)) // 返回 32 。最后 2 个数字的乘积是 4 * 8 = 32
 }
