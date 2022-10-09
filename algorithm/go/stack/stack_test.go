37170d6263a89a28197afb93470ebf92a446ba5c
zhanglihua.river <zhanglihua.river@bytedance.com>
2022-10-09 12:22:30 +0800

    921-使括号有效的最少添加

--- algorithm/go/stack/stack_test.go
+++ algorithm/go/stack/stack_test.go
@@ -8,3 +8,10 @@ import (
 func TestIsValid(t *testing.T) {
 	Validate(isValid("(])"), false, t)
 }
+
+func TestMinAddToMakeValid(t *testing.T) {
+	//Validate(minAddToMakeValid("())"), 1, t)
+	//Validate(minAddToMakeValid("((("), 3, t)
+	//Validate(minAddToMakeValid(""), 0, t)
+	Validate(minAddToMakeValid("()))(("), 4, t)
+}
