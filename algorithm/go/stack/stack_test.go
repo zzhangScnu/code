0d6788cd03c84ef72c7ca47d473010e06516cc48
zhanglihua.river <zhanglihua.river@bytedance.com>
2022-10-09 19:31:10 +0800

    1541-平衡括号字符串的最少插入次数

--- algorithm/go/stack/stack_test.go
+++ algorithm/go/stack/stack_test.go
@@ -10,8 +10,16 @@ func TestIsValid(t *testing.T) {
 }
 
 func TestMinAddToMakeValid(t *testing.T) {
-	//Validate(minAddToMakeValid("())"), 1, t)
-	//Validate(minAddToMakeValid("((("), 3, t)
-	//Validate(minAddToMakeValid(""), 0, t)
+	Validate(minAddToMakeValid("())"), 1, t)
+	Validate(minAddToMakeValid("((("), 3, t)
+	Validate(minAddToMakeValid(""), 0, t)
 	Validate(minAddToMakeValid("()))(("), 4, t)
+}
+
+func TestMinInsertions(t *testing.T) {
+	Validate(minInsertions("(()))"), 1, t)
+	Validate(minInsertions("())"), 0, t)
+	Validate(minInsertions("))())("), 3, t)
+	Validate(minInsertions("(((((("), 12, t)
+	Validate(minInsertions(")))))))"), 5, t)
 }
