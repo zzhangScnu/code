7e85415afaa6c0cf996b166178960bec978a744d
zhanglihua.river <zhanglihua.river@bytedance.com>
2022-10-12 12:20:24 +0800

    150-逆波兰表达式求值

--- algorithm/go/stack/stack_test.go
+++ algorithm/go/stack/stack_test.go
@@ -37,3 +37,9 @@ func TestSimplifyTest(t *testing.T) {
 	Validate(simplifyPath("/a/./b/../../c/"), "/c", t)
 	Validate(simplifyPath("/home//foo/"), "/home/foo", t)
 }
+
+func TestEvalRPN(t *testing.T) {
+	Validate(evalRPN([]string{"2", "1", "+", "3", "*"}), 9, t)
+	Validate(evalRPN([]string{"4", "13", "5", "/", "+"}), 6, t)
+	Validate(evalRPN([]string{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"}), 22, t)
+}
