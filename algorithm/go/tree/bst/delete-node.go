e8c86edf014562ee933546098099137cf469a8dd
zhanglihua.river <zhanglihua.river@bytedance.com>
2022-11-01 10:01:22 +0800

    450-删除二叉搜索树中的节点

---
+++ algorithm/go/tree/bst/delete-node.go
@@ -0,0 +1,90 @@
+package bst
+
+import . "algorithm.com/structure"
+
+//给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的 key 对应的节点，并保证二叉搜索树的性质不变。返回二叉搜索树（有可能被更新）的
+//根节点的引用。
+//
+// 一般来说，删除节点可分为两个步骤：
+//
+//
+// 首先找到需要删除的节点；
+// 如果找到了，删除它。
+//
+//
+//
+//
+// 示例 1:
+//
+//
+//
+//
+//输入：root = [5,3,6,2,4,null,7], key = 3
+//输出：[5,4,6,2,null,null,7]
+//解释：给定需要删除的节点值是 3，所以我们首先找到 3 这个节点，然后删除它。
+//一个正确的答案是 [5,4,6,2,null,null,7], 如下图所示。
+//另一个正确答案是 [5,2,6,null,4,null,7]。
+//
+//
+//
+//
+// 示例 2:
+//
+//
+//输入: root = [5,3,6,2,4,null,7], key = 0
+//输出: [5,3,6,2,4,null,7]
+//解释: 二叉树不包含值为 0 的节点
+//
+//
+// 示例 3:
+//
+//
+//输入: root = [], key = 0
+//输出: []
+//
+//
+//
+// 提示:
+//
+//
+// 节点数的范围 [0, 10⁴].
+// -10⁵ <= Node.val <= 10⁵
+// 节点值唯一
+// root 是合法的二叉搜索树
+// -10⁵ <= key <= 10⁵
+//
+//
+//
+//
+// 进阶： 要求算法时间复杂度为 O(h)，h 为树的高度。
+//
+// Related Topics 树 二叉搜索树 二叉树
+
+func deleteNode(root *TreeNode, key int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == key {
+		// 左右子树要是有一个是空的话，就用另一个接替父节点（包括了两个都为空的情况）
+		if root.Right == nil {
+			return root.Left
+		}
+		if root.Left == nil {
+			return root.Right
+		}
+		cur := root.Right
+		for cur.Left != nil {
+			cur = cur.Left
+		}
+		// 把右子树的最小值赋给根节点
+		root.Val = cur.Val
+		// 递归地去右子树里删除这个接替根节点的最小值的节点
+		root.Right = deleteNode(root.Right, cur.Val)
+	} else if root.Val > key {
+		root.Left = deleteNode(root.Left, key)
+	} else if root.Val < key {
+		root.Right = deleteNode(root.Right, key)
+	}
+	// 找不到的时候不删除，原样返回
+	return root
+}
