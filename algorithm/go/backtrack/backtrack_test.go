80b7a32cc8c4ec25cc7290f9e5d337bc75574a9a
zhanglihua.river <zhanglihua.river@bytedance.com>
2022-11-11 09:23:52 +0800

    47-全排列II

--- algorithm/go/backtrack/backtrack_test.go
+++ algorithm/go/backtrack/backtrack_test.go
@@ -2,10 +2,21 @@ package backtrack
 
 import (
 	. "algorithm.com/structure"
+	"fmt"
 	"testing"
 )
 
+// 候选集无重复，不可重复选
+// 全排列
 func TestPermute(t *testing.T) {
 	res := permute([]int{1, 2, 3})
 	Validate(len(res), 6, t)
+}
+
+// 候选集有重复，不可重复选
+// 全排列
+func TestPermuteUnique(t *testing.T) {
+	res := permuteUnique([]int{1, 1, 2})
+	fmt.Println(res)
+	Validate(len(res), 3, t)
 }
---
+++ algorithm/go/backtrack/permute-unique.go
@@ -0,0 +1,70 @@
+package backtrack
+
+import "sort"
+
+//给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：nums = [1,1,2]
+//输出：
+//[[1,1,2],
+// [1,2,1],
+// [2,1,1]]
+//
+//
+// 示例 2：
+//
+//
+//输入：nums = [1,2,3]
+//输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 8
+// -10 <= nums[i] <= 10
+//
+//
+// Related Topics 数组 回溯
+
+const EmptyNum = 11
+
+func permuteUnique(nums []int) [][]int {
+	if len(nums) == 1 {
+		return [][]int{nums}
+	}
+	sort.Ints(nums)
+	var res [][]int
+	var track []int
+	used := make([]bool, len(nums))
+	var dfs func()
+	dfs = func() {
+		if len(track) == len(nums) {
+			res = append(res, append([]int{}, track...))
+			return
+		}
+		preNum := EmptyNum // 在每一层，需要记录从左到右生成树枝的值，如果重复了就咔嚓
+		// 因为如果有树枝重复，下面生成的全排列子树必然也是会重复的，所以不需要考虑了
+		for i := 0; i < len(nums); i++ {
+			if used[i] == true || nums[i] == preNum {
+				continue
+			}
+			used[i] = true
+			preNum = nums[i]
+			track = append(track, nums[i])
+			dfs()
+			used[i] = false
+			//preNum = EmptyNum 不需要
+			track = track[:len(track)-1]
+		}
+	}
+	dfs()
+	return res
+}
