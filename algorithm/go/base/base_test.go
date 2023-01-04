eb794aade944456defde8341bf096d37801c1a4f
zhanglihua.river <zhanglihua.river@bytedance.com>
2023-01-04 09:24:42 +0800

    409-最长回文串

--- algorithm/go/base/base_test.go
+++ algorithm/go/base/base_test.go
@@ -1,41 +1,42 @@
 package base
 
 import (
-	"algorithm.com/structure"
+	. "algorithm.com/structure"
 	"testing"
 )
 
 func TestConvertToZ(t *testing.T) {
 	res := Convert("ABCDE", 4)
-	structure.Validate(res, "ABCED", t)
+	Validate(res, "ABCED", t)
 }
 
 func TestRomanToInt(t *testing.T) {
 	res := RomanToInt("MCMXCIV")
-	structure.Validate(res, 1994, t)
+	Validate(res, 1994, t)
 }
 
 func TestIntToRoman(t *testing.T) {
 	res := IntToRoman(20)
-	structure.Validate(res, "XX", t)
+	Validate(res, "XX", t)
 }
 
 func TestAddBinary(t *testing.T) {
-	structure.Validate(AddBinary("11", "1"), "100", t) // 7
-	structure.Validate(AddBinary("0", "1"), "1", t)
-	structure.Validate(AddBinary("1111", "1111"), "11110", t)
+	Validate(AddBinary("11", "1"), "100", t) // 7
+	Validate(AddBinary("0", "1"), "1", t)
+	Validate(AddBinary("1111", "1111"), "11110", t)
 	// todo: 这个用例会上溢
-	structure.Validate(AddBinary("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101", "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"), "110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000", t)
-}
-
-func TestMaxArea(t *testing.T) {
-	structure.Validate(MaxArea([]int{1, 8, 6, 2, 5, 4, 8, 3, 7}), 49, t)
-	structure.Validate(MaxArea([]int{1, 1}), 1, t)
+	Validate(AddBinary("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101", "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"), "110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000", t)
 }
 
 func TestLongestCommonPrefix(t *testing.T) {
-	structure.Validate(LongestCommonPrefix([]string{"flower", "flow", "flight"}), "fl", t)
-	structure.Validate(LongestCommonPrefix([]string{"dog", "racecar", "car"}), "", t)
-	structure.Validate(LongestCommonPrefix([]string{""}), "", t)
-	structure.Validate(LongestCommonPrefix([]string{"a"}), "a", t)
+	Validate(LongestCommonPrefix([]string{"flower", "flow", "flight"}), "fl", t)
+	Validate(LongestCommonPrefix([]string{"dog", "racecar", "car"}), "", t)
+	Validate(LongestCommonPrefix([]string{""}), "", t)
+	Validate(LongestCommonPrefix([]string{"a"}), "a", t)
+}
+
+func TestLongestPalindrome(t *testing.T) {
+	Validate(longestPalindrome("abccccdd"), 7, t)
+	Validate(longestPalindrome("ccc"), 3, t)
+	Validate(longestPalindrome("cccbb"), 5, t)
 }
---
+++ algorithm/go/base/longest-palindrome.go
@@ -0,0 +1,59 @@
+package base
+
+//给定一个包含大写字母和小写字母的字符串
+// s ，返回 通过这些字母构造成的 最长的回文串 。
+//
+// 在构造过程中，请注意 区分大小写 。比如 "Aa" 不能当做一个回文字符串。
+//
+//
+//
+// 示例 1:
+//
+//
+//输入:s = "abccccdd"
+//输出:7
+//解释:
+//我们可以构造的最长的回文串是"dccaccd", 它的长度是 7。
+//
+//
+// 示例 2:
+//
+//
+//输入:s = "a"
+//输入:1
+//
+//
+// 示例 3：
+//
+//
+//输入:s = "aaaaaccc"
+//输入:7
+//
+// 提示:
+//
+//
+// 1 <= s.length <= 2000
+// s 只由小写 和/或 大写英文字母组成
+//
+//
+// Related Topics 贪心 哈希表 字符串
+
+func longestPalindrome(s string) int {
+	mapping := make(map[rune]int, len(s))
+	for _, si := range s {
+		mapping[si] += 1
+	}
+	length := 0
+	usedSingle := false
+	for _, cnt := range mapping {
+		if cnt%2 == 0 {
+			length += cnt
+		} else if !usedSingle {
+			length += cnt
+			usedSingle = true
+		} else {
+			length += cnt / 2 * 2
+		}
+	}
+	return length
+}
