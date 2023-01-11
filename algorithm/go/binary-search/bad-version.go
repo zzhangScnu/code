ae2811e4528c33d43a424c3256fdcc9393a83c36
zhanglihua.river <zhanglihua.river@bytedance.com>
2023-01-11 20:22:38 +0800

    278-第一个错误的版本

--- algorithm/go/binary-search/bad-version.go
+++ algorithm/go/binary-search/bad-version.go
@@ -2,25 +2,15 @@ package binary_search
 
 func firstBadVersion(n int) int {
 	low, high, mid := 1, n, 0
-	for low <= high {
+	for low < high { // 因为一定会找到，而取等时的跳出条件是low > high，不可能到达这个条件，会死循环
 		mid = low + (high-low)>>1
 		if isBadVersion(mid) {
-			badVersionIdx := mid
-			shrinkMid := low + (badVersionIdx-low)>>1
-			if isBadVersion(shrinkMid) {
-				badVersionIdx = shrinkMid
-			}
-			for badVersionIdx >= 0 {
-				if isBadVersion(badVersionIdx); badVersionIdx == 0 || !isBadVersion(badVersionIdx-1) {
-					return badVersionIdx
-				}
-				badVersionIdx--
-			}
+			high = mid
 		} else {
-			low = mid + (high-mid)>>1
+			low = mid + 1
 		}
 	}
-	return -1
+	return low
 }
 
 func isBadVersion(version int) bool {
